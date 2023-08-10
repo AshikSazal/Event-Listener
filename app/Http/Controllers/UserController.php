678e5910d1f3490ef0e9c7ce0d1aa0beff469950
AshikSazal <sojolislm@gmail.com>
2023-08-10 21:27:45 +0600

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function register(Request $request){
+        $user = User::create([
+            'name'=>$request->input('name'),
+            'email'=>$request->input('email'),
+            'password'=>$request->input('password'),
+        ]);
+
+        $user->save();
+    }
+}
